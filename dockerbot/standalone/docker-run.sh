ec8f1dfee1abe0710ccdef7fcf28f08c01d4c932
Tulir Asokan <tulir@maunium.net>
2020-01-15 23:07:14 +0200

    Add example config for standalone image

--- dockerbot/standalone/docker-run.sh
+++ dockerbot/standalone/docker-run.sh
@@ -6,14 +6,14 @@ function fixperms {
 
 cd /opt/maubot/dockerbot
 
-#if [ ! -f /data/config.y>aml ]; then
-#	cp supportportal/standalone/example-config.yaml /data/config.yaml
-#	echo "Config file not found. Example config copied to /data/config.yaml"
-#	echo "Please modify the config file to your liking and restart the container."
-#	fixperms
-#	exit
-#fi
+if [ ! -f /data/config.yaml ]; then
+	cp dockerbot/standalone/example-config.yaml /data/config.yaml
+	echo "Config file not found. Example config copied to /data/config.yaml"
+	echo "Please modify the config file to your liking and restart the container."
+	fixperms
+	exit
+fi
 
 fixperms
-exec su-exec $UID:$GID python3 -m maubot.standalone -m maubot.yaml
-  # -c /data/config.yaml -b dockerbot/standalone/example-config.yaml
+exec su-exec $UID:$GID python3 -m maubot.standalone -m maubot.yaml \
+  -c /data/config.yaml -b dockerbot/standalone/example-config.yaml
